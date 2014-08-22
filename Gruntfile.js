52bd30e0cb2deb276bf5f5d765c0b1130e0a40da
Igor Shevchenko <cyberskunk@gmail.com>
2014-08-23 00:02:30 +0400

    migrate building from ant to Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+var config = require('./package');
+
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: config,
+
+    uglify: {
+      build: {
+        src: 'dist/<%=pkg.name%>-<%=pkg.version%>/<%=pkg.name%>.js',
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>/<%=pkg.name%>.min.js',
+      }
+    },
+
+    clean: [ 'dist/' ],
+
+    copy: {
+      examples: {
+        src: 'examples/**',
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>/'
+      },
+      css: {
+        src: 'css/**',
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>/'
+      },
+      files: {
+        src: [ 'README.txt', '*-LICENSE.txt'],
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>/'
+      },
+      js: {
+        src: 'src/<%=pkg.name%>.js',
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>/<%=pkg.name%>.js',
+        options: {
+          process: function (content) {
+            return content.replace(/@VERSION/g, config.version);
+          }
+        }
+      }
+    },
+
+    zip: {
+      dist: {
+        cwd: 'dist/',
+        src: 'dist/<%=pkg.name%>-<%=pkg.version%>/**',
+        dest: 'dist/<%=pkg.name%>-<%=pkg.version%>.zip'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'copy', 'uglify', 'zip' ]);
+
+};
